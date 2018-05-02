e5723a2d30f312844e938078f8d581f58ea38115
mahdi shakibi <mahdi.shakibi@gmail.com>
2018-05-01 21:59:07 -0700

    add Passport.js

--- app.js
+++ app.js
@@ -25,6 +25,43 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+const passport = require('./passport');
+app.use(passport.initialize());
+app.use('/api/', passport.authenticate('jwt', {
+  session: false,
+  failWithError: true
+}));
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+  });
+  
+  // error handler
+  app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+  
+  async function shutdown(callback) {
+    await mongoose.disconnect();
+    if (callback) callback();
+    else
+      process.exit(0);
+  }
+  
+  process.on('SIGINT', shutdown);
+  process.on('SIGTERM', shutdown);
+  process.once('SIGUSR2', () => {
+    shutdown(() => process.kill(process.pid, 'SIGUSR2'));
+  });
+
 app.listen(3000, () => console.log('Listening on port 3000'));
 
 
