cd52c5afcb5ad54bc25996d1a2a916446bfbac99
Unknown <alvinquach91@gmail.com>
2018-05-07 12:51:54 -0700

    jwt and position authentication working

--- routes/login.js
+++ routes/login.js
@@ -4,27 +4,23 @@ const bcrypt = require('bcrypt');
 const express = require('express');
 const router = express.Router();
 
-const invalidError = {
-    status: 401,
-    message: "Invalid username or password."
-};
+const sendUnauthorized = (res) => {
+    res.status(401).send("Invalid username or password.");
+}
 
 router.post('/', function(req, res, next) {
     const username = req.query.username;
     const password = req.query.password;
     if (!username || !password) {
-        next(invalidError);
-        return;
+        return sendUnauthorized(res);
     }
     User.findOne({username: username}, (err, user) => {
         if (!user) {
-            next(invalidError);
-            return;
+            return sendUnauthorized(res);
         }
         bcrypt.compare(password, user.hash, (err, same) => {
             if (err || !same) {
-                next(invalidError);
-                return;
+                return sendUnauthorized(res);
             }
             const payload = {
                 id: user._id,
