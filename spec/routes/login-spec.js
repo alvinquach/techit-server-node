34ea86c9c5aa86a2944076187188faeeb2324f88
Unknown <alvinquach91@gmail.com>
2018-05-07 18:52:17 -0700

    added login and user tests

---
+++ spec/routes/login-spec.js
@@ -0,0 +1,42 @@
+const request = require('request');
+const authentication = require('../../authentication/authentication');
+
+const loginApi = request.defaults({
+    baseUrl: 'http://localhost:3000/login',
+    json: true
+});
+
+describe('Login API Tests:', () => {
+
+    describe("POST / :", () => {
+
+        it("Status 200 expected", (done) => {
+            loginApi.post({
+                url: '/',
+                qs: {
+                    username: 'techit',
+                    password: 'abcd'
+                }
+            }, (err, res, body) => {
+                expect(res.statusCode).toBe(200);
+                expect(body.startsWith("Bearer ")).toBe(true);
+                done();
+            });
+        });
+
+        it("Status 401 expected", (done) => {
+            loginApi.post({
+                url: '/',
+                qs: {
+                    username: 'techit',
+                    password: 'wrong password'
+                }
+            }, (err, res, body) => {
+                expect(res.statusCode).toBe(401);
+                done();
+            });
+        });
+
+    });
+
+});
